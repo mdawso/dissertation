3e25c6cfab5e21a32f5f60e5905d421620c761ba
bazze08 <dawsonmathew08@gmail.com>
2024-11-14 01:53:42 +0000

    the ai can now see tiles around it
    this may be a bad way of doing this

--- scripts/ai_player.gd
+++ scripts/ai_player.gd
@@ -7,6 +7,8 @@ const TCP_PORT : int = 9876
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+const SIGHT_RANGE = 2
+
 @onready var state_label: Label = %StateLabel
 
 func _ready() -> void:
@@ -27,6 +29,20 @@ func _physics_process(delta: float) -> void:
 	
 	if peer.get_status() == 2:
 		
+		# send data to python
+		var visibleTiles = {}
+		var playerPos = Globals.Map.local_to_map(position)
+		
+		for x in range(playerPos.x - SIGHT_RANGE, playerPos.x + SIGHT_RANGE + 1): # godot is really annoying here, +1 because the max is exclusive
+			for y in range(playerPos.y - SIGHT_RANGE, playerPos.y + SIGHT_RANGE + 1):
+				var coordinates = Vector2i(x,y)
+				var cellTileData = Globals.Map.get_cell_tile_data(coordinates)
+				if cellTileData:
+					if cellTileData.get_collision_polygons_count(0) > 0:
+						visibleTiles[coordinates] = 1
+				else:
+					visibleTiles[coordinates] = 0
+		
 		# recieve data from python and process it
 		var available_bytes = peer.get_available_bytes()
 		if available_bytes > 0:
@@ -42,12 +58,7 @@ func _physics_process(delta: float) -> void:
 				velocity.x = SPEED
 			elif data.move == "jump" and is_on_floor():
 				velocity.y = JUMP_VELOCITY
-			
-			peer.put_data("godot processed data".to_ascii_buffer())
-			
-		# send back response
-		# TODO send data
-		# tiles around player
-		# distance to goal
+				
+			peer.put_data("Got it!".to_ascii_buffer())
 
 	move_and_slide()
